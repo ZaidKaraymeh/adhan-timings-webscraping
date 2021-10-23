6b728a015f72217a80c04e63922fbeaac86811ae
ZaidKaraymeh <78732125+ZaidKaraymeh@users.noreply.github.com>
2021-10-23 23:21:16 +0300

    Update adhan.py

--- adhan.py
+++ adhan.py
@@ -1,35 +1,5 @@
 # # from apscheduler.schedulers.background import BackgroundScheduler
 
-# import collections
-# import requests
-# from bs4 import BeautifulSoup
-
-# # timetabke table table-striped
-# # id = timetable
-
-# url = "https://www.edarabia.com/prayer-times-bahrain/"
-# page = requests.get(url)
-# soup = BeautifulSoup(page.content, "html.parser")
-# # results gets timings for the month
-# results = soup.find(id="timetable")
-
-# # timings gets timing rows
-# timings = results.find_all("tr", class_="")
-
-# # print(type(timings))
-
-# for time in timings:
-#     time_to_string = collections.defaultdict(set)
-#     print(type(time))
-#     splitter = str(time.split("m"))
-#     print(splitter.text, end="\n"*2)
-
-
-# # print(results.prettify())
-# # def adhan_scraper():
-# #     pass
-
-
 
 # # scheduler = BackgroundScheduler()
 # # scheduler.add_job(adhan_scraper, 'interval', hours=24)
